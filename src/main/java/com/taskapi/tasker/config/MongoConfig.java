cefb74b8aae23024d636b5776411ebeaa19f7c4d
PB <xachineon@gmail.com>
2026-03-23 12:54:48 +0530

    Fixing Mongo issue

--- src/main/java/com/taskapi/tasker/config/MongoConfig.java
+++ src/main/java/com/taskapi/tasker/config/MongoConfig.java
@@ -1,15 +1,30 @@
 package com.taskapi.tasker.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 
 
 @Configuration 
 
-@EnableMongoAuditing 
+//@EnableMongoAuditing 
 
 public class MongoConfig { 
+	 private static final String MONGO_URI = "mongodb+srv://tasker_user:a2RgHJWz7b3C68PC@task.kzvmvis.mongodb.net/tasker_audit?retryWrites=true&w=majority&appName=Task&authSource=admin";
 
+	    @Bean
+	     MongoClient mongoClient() {
+	        return MongoClients.create(MONGO_URI);
+	    }
+
+	    @Bean
+	     MongoTemplate mongoTemplate() {
+	        return new MongoTemplate(mongoClient(), "tasker_audit");
+	    }
+	
    // Spring Boot auto-configures the MongoClient from application.yml. 
 
    // Repository scanning is handled by @SpringBootApplication on the main class. 
